86c87a8bd5e79508981c5fbda242f04b21f34bc8
Gabriele Giunchi <gabriele.giunchi1994@gmail.com>
2021-09-28 09:17:15 +0200

    inject store

--- src/lib/components/Child.js
+++ src/lib/components/Child.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Component2 from "./Component2";
 import Logo from '../logo.svg';
+import {Provider} from "react-redux";
+import store from "../redux/store";
 
 export const Child = () => {
-    return <div>
+    return <Provider store={store}>
         <img alt="logo" width="200px" src={Logo}/>
         <p>child</p>
         <Component2/>
-    </div>
+    </Provider>
 }
