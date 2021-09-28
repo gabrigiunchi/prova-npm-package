1d054f5ae3d987bdfa5666ed92ab2603cd97ceef
Gabriele Giunchi <gabriele.giunchi1994@gmail.com>
2021-09-28 08:10:24 +0200

    add logo

--- src/lib/components/Child.js
+++ src/lib/components/Child.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import {Component2} from "./Component2";
+import Logo from '../logo.svg';
 
 export const Child = () => {
     return <div>
+        <img width="200px" src={Logo}/>
         <p>child</p>
         <Component2/>
     </div>
