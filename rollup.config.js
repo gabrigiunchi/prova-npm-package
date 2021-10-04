5cac91c909c8dc7d41aa88efc3879e496eed5bd9
Gabriele Giunchi <gabriele.giunchi1994@gmail.com>
2021-10-04 21:20:37 +0200

    add babel config

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,7 @@
 import babel from '@rollup/plugin-babel';
-import {nodeResolve} from '@rollup/plugin-node-resolve';
+import resolve, {nodeResolve} from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import image from '@rollup/plugin-image';
-import resolve from '@rollup/plugin-node-resolve';
 
 export default {
     input: 'src/lib/index.js',
@@ -15,7 +14,7 @@ export default {
         image(),
         nodeResolve(),
         babel({
-            presets: ["@babel/preset-react"],
+            presets: ["@babel/preset-react", "@babel/env"],
         }),
         commonjs(),
     ]
