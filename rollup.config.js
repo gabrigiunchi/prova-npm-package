b26027d96a5bd0277725319107230643a9680b59
Gabriele Giunchi <gabriele.giunchi1994@gmail.com>
2021-10-04 21:19:54 +0200

    adjust pipeline

--- rollup.config.js
+++ rollup.config.js
@@ -4,19 +4,16 @@ import commonjs from '@rollup/plugin-commonjs';
 import image from '@rollup/plugin-image';
 import resolve from '@rollup/plugin-node-resolve';
 
-
 export default {
     input: 'src/lib/index.js',
     output: {
-        file: 'bundle.js',
+        file: 'dist/index.js',
         format: 'cjs'
     },
     plugins: [
         resolve(),
         image(),
-        nodeResolve({
-            extensions: [".js"],
-        }),
+        nodeResolve(),
         babel({
             presets: ["@babel/preset-react"],
         }),
