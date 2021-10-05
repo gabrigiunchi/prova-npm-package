71e5bf243895052b96654918d1a0ed452978ef8a
Gabriele Giunchi <gabriele.giunchi1994@gmail.com>
2021-10-05 09:15:54 +0200

    change rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,8 @@
 import babel from '@rollup/plugin-babel';
-import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import image from '@rollup/plugin-image';
 import json from '@rollup/plugin-json';
-import nodeResolve from "@rollup/plugin-node-resolve";
+import resolve from "@rollup/plugin-node-resolve";
 
 export default {
     input: 'src/lib/index.js',
@@ -18,7 +17,7 @@ export default {
             babelHelpers: 'bundled',
             presets: ["@babel/preset-react"],
         }),
-        nodeResolve({extensions: [".js"]}),
+        resolve(),
         commonjs(),
         json(),
         image(),
