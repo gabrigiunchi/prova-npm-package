82e335ac2edf48b5246076bb463e7e7204a649d0
Gabriele Giunchi <gabriele.giunchi1994@gmail.com>
2021-10-05 18:51:06 +0200

    modify rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,6 @@ import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import image from '@rollup/plugin-image';
 import json from '@rollup/plugin-json';
-import resolve from "@rollup/plugin-node-resolve";
 
 export default {
     input: 'src/lib/index.js',
@@ -10,13 +9,12 @@ export default {
         {
             file: 'dist/index.js',
             format: 'cjs',
-         }
+        }
     ],
     plugins: [
         babel({
             babelHelpers: 'bundled'
         }),
-        resolve(),
         commonjs(),
         json(),
         image(),
