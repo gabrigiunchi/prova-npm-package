653809e2a733bf3337a1051f4219a5b0528106a0
Gabriele Giunchi <gabriele.giunchi1994@gmail.com>
2021-10-06 12:24:31 +0200

    remove sourcemap

--- rollup.config.js
+++ rollup.config.js
@@ -11,7 +11,6 @@ export default {
         {
             file: packageJson.main,
             format: 'cjs',
-            sourcemap: true
         },
         {
             file: packageJson.main,
