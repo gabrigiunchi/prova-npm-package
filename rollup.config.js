dc83378348ac4b5cbeeae8044f15127fa6ccba12
Gabriele Giunchi <gabriele.giunchi1994@gmail.com>
2021-10-06 12:17:09 +0200

    add cjs format

--- rollup.config.js
+++ rollup.config.js
@@ -3,12 +3,19 @@ import commonjs from '@rollup/plugin-commonjs';
 import image from '@rollup/plugin-image';
 import json from '@rollup/plugin-json';
 
+const packageJson = require('./package.json');
+
 export default {
     input: 'src/lib/index.js',
     output: [
         {
-            file: 'dist/index.js',
+            file: packageJson.main,
             format: 'cjs',
+            sourcemap: true
+        },
+        {
+            file: packageJson.main,
+            format: 'esm',
             sourcemap: true
         }
     ],
