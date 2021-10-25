2ecf9c91ba7469f21cbbeaaf673efdb4ea8cfd5b
Gabriele Giunchi <gabriele.giunchi1994@gmail.com>
2021-10-25 10:13:21 +0200

    replace env variables

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,18 @@ import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import image from '@rollup/plugin-image';
 import json from '@rollup/plugin-json';
+import replace from '@rollup/plugin-replace';
+import {nodeResolve} from '@rollup/plugin-node-resolve';
+
+const environment = process.env.NODE_ENV;
+let path = '.env';
+if (environment) {
+    path += `.${environment}`
+}
+require('dotenv').config({path});
+const replaceVariables = Object.entries(process.env)
+    .map(entry => ({[`process.env.${entry[0]}`]: entry[1]}))
+    .reduce((a, b) => ({...a, ...b}), {});
 
 const packageJson = require('./package.json');
 
@@ -11,10 +23,15 @@ export default {
     output: [
         {
             file: packageJson.main,
-            format: 'esm',
+            format: 'esm'
         }
     ],
     plugins: [
+        nodeResolve({extensions: ['.js']}),
+        replace({
+            preventAssignment: true,
+            ...replaceVariables,
+        }),
         babel({
             babelHelpers: 'bundled'
         }),
