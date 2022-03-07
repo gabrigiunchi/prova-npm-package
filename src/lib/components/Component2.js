55d9378ba51c77af0c2bb38cfd697500ac32a370
Gabriele Giunchi <gabriele.giunchi1994@gmail.com>
2022-03-07 14:24:40 +0100

    set background

--- src/lib/components/Component2.js
+++ src/lib/components/Component2.js
@@ -1,12 +1,13 @@
 import React, {useState} from 'react';
 import {foo} from "../services/fooService";
 import {connect} from "react-redux";
+import * as classes from './Component2.module.css';
 
 const Component2 = ({array}) => {
 
     const [s] = useState(foo());
 
-    return <div>
+    return <div className={classes.container}>
         <p>component2</p>
         <p>array length: {array?.length}</p>
         <p>{s}</p>
