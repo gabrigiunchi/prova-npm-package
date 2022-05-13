0d610ab5bacc9f76cb03cabea12a9331fe1be5bd
Gabriele Giunchi <gabriele.giunchi1994@gmail.com>
2022-05-13 09:00:22 +0200

    remove loggerutil

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,9 +1,4 @@
 import {Child} from "./components/Child";
 import './i18n';
-import {disabledLogger} from "./utils/loggerUtils";
-
-if (process.env.NODE_ENV === 'production') {
-    disabledLogger();
-}
 
 export {Child};
